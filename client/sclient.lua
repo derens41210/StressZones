203e5c74d330d89ac9466b2bf232858cf866896d
derens <46387744+derens41210@users.noreply.github.com>
2022-07-15 20:01:31 +0200

    v1.0
    Created the plugin for fivem of the anti stress zones.
    Works for esx and qbcore

---
+++ client/sclient.lua
@@ -0,0 +1,53 @@
+-- ╔════╗  ╔══════ ╔════╗   ╔══════ ╔╗   ║ ╔══════
+-- ║    ╚╗ ║       ║    ╚╗  ║       ║╚╗  ║ ║
+-- ║     ║ ╠═══    ╠╦════╝  ╠═══    ║ ╚╗ ║ ╚═════╗
+-- ║    ╔╝ ║       ║╚═╗     ║       ║  ╚╗║       ║
+-- ╚════╝  ╚══════ ║  ╚═╗	╚══════ ║   ╚╝ ══════╝
+-- Version 1.0 
+
+if Config.esxFrame == true then
+	ESX = nil
+	local segundosTotal = Config.segundosDelay * 1000
+
+	CreateThread(function()
+		local currentGasBlip = 0
+		while true do
+			local coordenadasUsuario = GetEntityCoords(PlayerPedId())
+			local distanciaMax = Config.distanciaZona
+			
+			for _, zonaAntiEstres in pairs(Config.stressZones) do
+				local distanciaZona = #(coordenadasUsuario - zonaAntiEstres)
+
+				if distanciaZona < distanciaMax then
+					print(Config.cantidadStressReducir)
+					TriggerEvent('esx_status:remove', 'stress', Config.cantidadStressReducir)
+					TriggerEvent("pNotify:SendNotification", {text = Config.strings.menssageStressDown})
+				end
+			end
+
+			Wait(segundosTotal)
+		end
+	end)
+else
+	local QBCore = exports['qb-core']:GetCoreObject()
+	local segundosTotal = Config.segundosDelay * 1000
+
+	CreateThread(function()
+		local currentGasBlip = 0
+		while true do
+			local coordenadasUsuario = GetEntityCoords(PlayerPedId())
+			local distanciaMax = Config.distanciaZona
+			
+			for _, zonaAntiEstres in pairs(Config.stressZones) do
+				local distanciaZona = #(coordenadasUsuario - zonaAntiEstres)
+
+				if distanciaZona < distanciaMax then
+					TriggerServerEvent('hud:server:RelieveStress', Config.cantidadStressReducir)
+				end
+			end
+
+			Wait(segundosTotal)
+		end
+	end)
+
+end
---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'derens41210 <fivem@derhos.es>'
+description 'plugin to set anti-stress zones'
+version '1.0'
+
+shared_script 'config.lua'
+
+client_scripts {
+	'client/sclient.lua'
+}
